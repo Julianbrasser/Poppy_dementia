856146e233fd5ade799111a875f7f590b967a040
Julian-Brasser <julianbrasser.jb@gmail.com>
2017-03-27 12:04:30 +0200

    Added content

---
+++ index.py
@@ -0,0 +1,18 @@
+from poppy.creatures import PoppyTorso
+import json
+
+my_robot = autodetect_robot()
+
+for m in my_robot.motors:
+    m.goal_position = 0.0
+
+
+import json
+
+config = my_robot.to_config()
+
+with open('my_robot.json', 'wb') as f:
+    json.dump(config, f)
+
+my_robot = from_json('my_robot.json')
+
